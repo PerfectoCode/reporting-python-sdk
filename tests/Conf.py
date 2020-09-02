8e3ca35717512651174aeb1ea1c36256f4672012
liorgd <LGdaliahu@perforce.com>
2020-09-02 09:51:25 +0300

    update

--- tests/Conf.py
+++ tests/Conf.py
@@ -20,8 +20,6 @@ class TestConf(unittest.TestCase):
         capabilities = {
             'platformName': 'Android',
             'deviceName': '',
-            'user': self.user,
-            'password': self.password,
             'securityToken':'eyJhbGciOiJIUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJlMWRkYTg4Mi0yMGQ4LTRmZjItYWE0YS1jNWQ2M2VjMWI0NWYifQ.eyJqdGkiOiIyNDhlYzE1Yy05OGZmLTRjMTEtYTY2Ny1lOGM2M2Y1MTk3Y2QiLCJleHAiOjAsIm5iZiI6MCwiaWF0IjoxNTk4OTQ0NjU3LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjkwOTAvYXV0aC9yZWFsbXMvcmVwb3J0aW5nLXN0YWdpbmctcGVyZmVjdG9tb2JpbGUtY29tIiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdDo5MDkwL2F1dGgvcmVhbG1zL3JlcG9ydGluZy1zdGFnaW5nLXBlcmZlY3RvbW9iaWxlLWNvbSIsInN1YiI6IjRkYTM3ZmJiLTYxZjEtNDIxNC04YTFlLWFhNzg2MzIyZmQwYiIsInR5cCI6Ik9mZmxpbmUiLCJhenAiOiJvZmZsaW5lLXRva2VuLWdlbmVyYXRvciIsImF1dGhfdGltZSI6MCwic2Vzc2lvbl9zdGF0ZSI6ImI2N2VkMTdiLWMzYmItNGM2MS1iNWIzLWJmOWE2NTA3YTI0NyIsInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJwcm9maWxlIG9mZmxpbmVfYWNjZXNzIGVtYWlsIn0.3nKxjZ_wmrdRchPsrps1Zm3UX8XnxMjVK1O0gy7Ag44'
         }
         self.driver = webdriver.Remote('https://' + self.host + '/nexperience/perfectomobile/wd/hub', capabilities)
